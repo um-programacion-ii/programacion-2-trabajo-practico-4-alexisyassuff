3ecabea5c39a9d957178a7837e463078d46add5c
alexisyassuff <a.yassuff@alumno.um.edu.ar>
2025-05-07 15:33:55 -0300

    Implementacion de LibroController

--- tp4/src/main/java/org/example/Controladores/LibroController.java
+++ tp4/src/main/java/org/example/Controladores/LibroController.java
@@ -1,4 +1,47 @@
 package org.example.Controladores;
+import org.example.Clases.Libro;
+import org.example.Repositorios.*;
+import org.example.Servicios.*;
+import org.springframework.web.bind.annotation.*;
 
-public class LibroControllers {
+import java.util.*;
+
+@RestController
+@RequestMapping("/api/libros")
+public class LibroController {
+    private final LibroService libroService;
+
+    public LibroController(LibroService libroService) {
+        this.libroService = libroService;
+    }
+
+    @GetMapping
+    public List<Libro> obtenerTodos() {
+        return libroService.obtenerTodosLibros();
+    }
+
+    @GetMapping("/{id}")
+    public Libro buscarPorIsbn(@PathVariable String id) {
+        return libroService.buscarPorIsbn(id);
+    }
+
+    @GetMapping("/{id}")
+    public Libro obtenerPorId(@PathVariable Long id) {
+        return libroService.buscarPorId(id);
+    }
+
+    @PostMapping
+    public Libro crear(@RequestBody Libro libro) {
+        return libroService.guardarLibro(libro);
+    }
+
+    @PutMapping("/{id}")
+    public Libro actualizar(@PathVariable Long id, @RequestBody Libro libro) {
+        return libroService.actualizarLibro(id, libro);
+    }
+
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Long id) {
+        libroService.eliminarLibro(id);
+    }
 }
